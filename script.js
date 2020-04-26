1e0d7c2b08d0e20dba4076be070af9a6a7431ad4
vertjames <ianandrewkirby@gmail.com>
2020-04-26 12:58:28 -0700

    changed a variable label and added more comments

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
     
-  // Set up all of our variables (length, character types (lowercase, uppercase, numbers, special), ):
+// Set up all of our variables (length, character types (lowercase, uppercase, numbers, special)):
   const passwordLength = parseInt(prompt('Enter a desired password length between 8 and 128 characters (longer = more secure).'))
 
     // Warning if user enters a number lower than 8 or more than 128:
@@ -20,33 +20,37 @@ function generatePassword () {
   const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   const numbers = '1234567890'
   const special = '!@#$%^&*()_'
-  let inProgressPassword = ''
+  let userChosenCharacterTypes = ''
   newPassword = ''
 
-  // Warning if user chooses no characters at all:
-  if (!charLowerConf && !charUpperConf && !charNumConf && !charSpecConf) {
-    alert('Please choose to include at least 1 of these character options: lowercase, uppercase, numbers, special characters.')
-    return
-  }
+    // Warning if user chooses no characters at all:
+    if (!charLowerConf && !charUpperConf && !charNumConf && !charSpecConf) {
+      alert('Please choose to include at least 1 of these character options: lowercase, uppercase, numbers, special characters.')
+      return
+    }
 
+// If/Then statments for user chosen character types ("IF the user chose this character type THEN add that character type to the string called userChosenCharacterTypes"):
     if (charLowerConf) {
-      inProgressPassword += lowercase;
+      userChosenCharacterTypes += lowercase;
   }
     if (charUpperConf) {
-      inProgressPassword += uppercase;
+      userChosenCharacterTypes += uppercase;
   }
     if (charNumConf) {
-      inProgressPassword += numbers;
+      userChosenCharacterTypes += numbers;
     }
     if (charSpecConf) {
-      inProgressPassword += special;
+      userChosenCharacterTypes += special;
     }  
 
-
+// Next run a for loop for the amount of times that there are characters in the user entered 'passwordLength' prompt:
     for (let i = 0; i < passwordLength; i++) {
-      let randomIndex = Math.floor(Math.random() * inProgressPassword.length)
-      newPassword += inProgressPassword[randomIndex]
+// Each time the loop runs we're going to randomly choose a character from the userChosenCharacterTypes variable:     
+      let randomIndex = Math.floor(Math.random() * userChosenCharacterTypes.length)
+// Then we're going to take that randomly chosen character and add it to the newPassword:
+      newPassword += userChosenCharacterTypes[randomIndex]
     }
+// Once the loop has run for the amount of time specified in passwordLength then we're going to return with the completed newPassword:
     return newPassword
 }
 
